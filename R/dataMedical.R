9192ebe52a302228b1dec4ea72404bd88029129a
Patricia Martinkova <martinkova@cs.cas.cz>
2016-12-08 15:06:31 +0100

    Popis dat, NEWS aj.
    Doplneni a zmena popisu funkci a dat, pridan soubor NEWS, v ui pridan
    odkaz na V1.0.0, verze prejmenovana na 1.1.0.

---
+++ R/dataMedical.R
@@ -0,0 +1,25 @@
+#' Dichotomous Data Set of Admission Test to Medical School
+#'
+#' @description The \code{dataMedical} data set consists of the responses of 3,204 subjects
+#' to admission test to medical school in the Czech republic.
+#' It contains 100 items A correct answer is coded as 1 and incorrect answer as 0.
+#'
+#' @usage data(dataMedical)
+#'
+#' @author
+#' Patricia Martinkova \cr
+#' Institute of Computer Science, The Czech Academy of Sciences \cr
+#' martinkova@cs.cas.cz \cr
+#'
+#' Jakub Houdek \cr
+#' Institute of Computer Science, The Czech Academy of Sciences \cr
+#' houdek.james@gmail.com \cr
+#'
+#' @keywords datasets
+#'
+#' @seealso \code{\link{dataMedicaltest}}, \code{\link{dataMedicalkey}}
+#'
+#' @format A \code{dataMedical} data frame consists of 3,204 observations on
+#' 100 variables. The columns represent dichotomously scored items of the test.
+"dataMedical"
+
---
+++ R/dataMedicaltest.R
@@ -0,0 +1,21 @@
+#' Data Set of Admission Test to Medical School
+#'
+#' @description The \code{dataMedicaltest} data set consists of the responses of 3,204 subjects to multiple-choice admission test to medical school in Czech republic. It contains 100 items . Possible answers were A, B, C, D, while any combination of these can be correct.
+#'
+#' @usage data(dataMedicaltest)
+#'
+#' @author
+#' Patricia Martinkova \cr
+#' Institute of Computer Science, The Czech Academy of Sciences \cr
+#' martinkova@cs.cas.cz \cr
+#'
+#' Jakub Houdek \cr
+#' Institute of Computer Science, The Czech Academy of Sciences \cr
+#' houdek.james@gmail.com \cr#'
+#'
+#' @keywords datasets
+#'
+#' @seealso \code{\link{dataMedical}}, \code{\link{dataMedicalkey}}
+#'
+#' @format A \code{dataMedicaltest} data frame consists of 3,204 observations on 100 variables. The columns represent answers of subject to items of an admission test.
+"dataMedicaltest"
