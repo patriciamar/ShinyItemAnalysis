70a3d72e2c07ccadbc02dad87e72760d1c2461b7
Adéla Drabinová <adela.drabinova@gmail.com>
2016-12-29 09:19:17 +0100

    doplneni Dependencies
    Doplneni nekterych funkci z ggplot2

--- R/imports.R
+++ R/imports.R
@@ -6,8 +6,10 @@
 #' @importFrom CTT score
 #' @importFrom deltaPlotR deltaPlot
 #' @importFrom ggplot2 aes aes_string element_blank element_line element_rect element_text geom_abline
-#' geom_histogram geom_line geom_point geom_text ggplot ggsave ggtitle labs scale_fill_manual scale_x_continuous
-#' scale_y_continuous stat_function theme theme_bw unit xlab xlim ylab ylim
+#' ggplot_build position_dodge geom_histogram geom_hline geom_line geom_point geom_ribbon geom_text ggplot
+#' ggsave ggtitle labs scale_color_manual scale_colour_manual scale_fill_manual scale_linetype_manual
+#' scale_shape_manual scale_size_continuous scale_x_continuous scale_x_discrete scale_y_continuous
+#' stat_function stat_summary theme theme_bw unit xlab xlim ylab ylim
 #' @importFrom graphics lines plot plot.new
 #' @importFrom grDevices dev.off png rainbow recordPlot
 #' @importFrom ltm ltm rasch tpm factor.scores
