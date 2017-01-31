f95ed0c7e6f9c1a5d68e1e8ec701cc0fa0909887
Patricia Martinkova <martinkova@cs.cas.cz>
2017-01-31 15:37:06 +0100

    dataMedical update
    description of dataMedical was updated, reference was added

--- R/dataMedicaltest.R
+++ R/dataMedicaltest.R
@@ -1,18 +1,27 @@
 #' Data Set of Admission Test to Medical School
 #'
-#' @description The \code{dataMedicaltest} data set consists of the responses of 3,204 subjects to multiple-choice admission test to medical school in Czech republic. It contains 100 items . Possible answers were A, B, C, D, while any combination of these can be correct.
+#' @description The \code{dataMedicaltest} data set consists of the responses of 3,204 subjects to multiple-choice admission test to medical school. It contains 100 items, possible answers were A, B, C, D, while any combination of these can be correct.
 #'
 #' @usage data(dataMedicaltest)
 #'
 #' @author
+#' Cestmir Stuka \cr
+#' First Faculty of Medicine, Charles University in Prague \cr
+#' 
+#' Martin Vejrazka \cr
+#' First Faculty of Medicine, Charles University in Prague \cr
+#'
 #' Patricia Martinkova \cr
 #' Institute of Computer Science, The Czech Academy of Sciences \cr
 #' martinkova@cs.cas.cz \cr
-#'
+#' 
 #' Jakub Houdek \cr
 #' Institute of Computer Science, The Czech Academy of Sciences \cr
 #' houdek.james@gmail.com \cr#'
 #'
+#' @references
+#' Stuka, C. Vejrazka, M., Martinkova, P. Komenda, M. and Stepanek, L. (2016). The Use of  Test and Item Analisis for Improvement of Tests. Workshop held at conference MEFANET, 2016, Brno, Czech Republic.
+#' 
 #' @keywords datasets
 #'
 #' @seealso \code{\link{dataMedical}}, \code{\link{dataMedicalkey}}
