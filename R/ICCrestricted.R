ec607159a155c811816622f413f8e32bddca8a12
Patricia Martinkova <patriciamar@users.noreply.github.com>
2021-02-02 17:02:53 +0100

    ICCrestricted()

--- R/ICCrestricted.R
+++ R/ICCrestricted.R
@@ -1,7 +1,7 @@
 #' Range-restricted reliability with intra-class correlation
 #'
-#' Estimates reliability with intra-class correlation for whole or
-#' range-restricted samples using REML.
+#' Function estimating reliability with intra-class correlation for the complete
+#' or for the range-restricted sample.
 #'
 #' @param Data \code{matrix} or \code{data.frame} which includes variables
 #'   describing ID of ratees (specified in \code{case}), ratings (specified in
@@ -26,31 +26,49 @@
 #'
 #' @returns A \code{data.frame} with the following columns: \item{n_sel}{number
 #'   of ratees selected/subsetted.} \item{prop_sel}{proportion of ratees
-#'   selected.} \item{dir}{direction of range-restriction.}
-#'   \item{var_ratee}{variance due to ratee, "true variance", between-group
-#'   variance.} \item{var_resid}{residual variance.} \item{var_total}{total
-#'   variance.} \item{ICC1}{single-rater inter-rater reliability.}
-#'   \item{ICC1_LCI}{lower bound of the confidence interval for \code{ICC1}.}
-#'   \item{ICC1_UCI}{upper bound of the confidence interval for \code{ICC1}.}
-#'   \item{ICC3}{multiple-rater inter-rater reliability.} \item{ICC3_LCI}{lower
-#'   bound of the confidence interval for \code{ICC3}.} \item{ICC3_UCI}{upper
-#'   bound of the confidence interval for \code{ICC3}.}
+#'   selected.} \item{dir}{direction of range-restriction. \code{NA} if range is
+#'   effectively not restricted (100% used).} \item{VarID}{variance due to
+#'   ratee, "true variance", between-group variance.} \item{VarResid}{residual
+#'   variance.} \item{VarTotal}{total variance.} \item{ICC1}{single-rater
+#'   inter-rater reliability.} \item{ICC1_LCI}{lower bound of the confidence
+#'   interval for \code{ICC1}.} \item{ICC1_UCI}{upper bound of the confidence
+#'   interval for \code{ICC1}.} \item{ICC3}{multiple-rater inter-rater
+#'   reliability.} \item{ICC3_LCI}{lower bound of the confidence interval for
+#'   \code{ICC3}.} \item{ICC3_UCI}{upper bound of the confidence interval for
+#'   \code{ICC3}.}
+#'
+#'
+#' @author Patricia Martinkova \cr
+#' Institute of Computer Science of the Czech Academy of Sciences \cr
+#' \email{martinkova@@cs.cas.cz} \cr
+#'
+#' Jan Netik \cr
+#' Institute of Computer Science of the Czech Academy of Sciences \cr
+#'
+#' @references
+#' Erosheva, E., Martinkova, P., & Lee, C. (2021a). When zero may not be zero: A
+#' cautionary note on the use of inter-rater reliability in evaluating grant
+#' peer review. Journal of the Royal Statistical Society - Series A. Accepted.
+#'
+#' Erosheva, E., Martinkova, P., & Lee, C. (2021b). Supplementary material for
+#' When zero may not be zero: A cautionary note on the use of inter-rater
+#' reliability in evaluating grant peer review.
+#' Retrieved from \url{osf.io/knph8}
 #'
 #' @examples
 #' # loading AIBS dataset
 #' data(AIBS, package = "ShinyItemAnalysis")
 #'
 #' # ICC for the whole sample
-#' ICCrestricted(
-#'   Data = AIBS, case = "ID", var = "Score", rank = "ScoreRankAdj"
-#' )
+#' ICCrestricted(Data = AIBS, case = "ID", var = "Score", rank = "ScoreRankAdj")
+#'
 #' # ICC for the range-restricted sample considering 80% of top ratees
-#' ICCrestricted(
-#'   Data = AIBS, case = "ID", var = "Score", rank = "ScoreRankAdj", sel = 0.8
-#' )
+#' ICCrestricted(Data = AIBS, case = "ID", var = "Score", rank = "ScoreRankAdj",
+#'               sel = 0.8)
+#'
 #' @importFrom lme4 lmer VarCorr bootMer
 #' @importFrom stats sigma
-#' @importFrom dplyr between arrange group_by ungroup mutate
+#' @importFrom dplyr arrange group_by ungroup mutate
 #' @importFrom tidyr nest unnest
 #' @importFrom tibble tibble rowid_to_column
 #' @importFrom stats quantile
@@ -60,44 +78,57 @@
 #'
 ICCrestricted <- function(Data, case, var, rank = NULL,
                           dir = "top", sel = 1, nsim = 100, ci = .95, seed = NULL) {
-  # if rank = NULL, compute rank based on Score
-  if (is.null(rank)) {
-    Data <- Data %>%
-      group_by(.data[[case]]) %>%
-      mutate(.ms = mean(.data[[var]], na.rm = TRUE)) %>%
-      arrange(.data[[".ms"]]) %>%
-      nest() %>%
-      rowid_to_column(".rank") %>%
-      unnest(cols = .data$data) %>%
-      ungroup()
+  if (is.null(Data[[case]])) stop("Case variable '", case, "' not present in Data.", call. = FALSE)
+  if (is.null(Data[[var]])) stop("Dependent variable '", var, "' not present in Data.", call. = FALSE)
 
-    rank <- ".rank"
+  sel_max <- Data[[case]] %>% unique() %>% length()
+
+  if (sel <= 1) {
+    sel <- round(sel * sel_max)
+    if (sel < 2) {
+      sel <- 2
+      warning(
+        "There must be at least 2 cases in the subset.\n",
+        "A minimum proportion, i.e., ", round(2 / sel_max, 3), " was used.",
+        call. = FALSE
+      )
+    }
   }
 
-  sel_max <- max(Data[[rank]], na.rm = TRUE)
+  if (sel != sel_max) {
+    if (is.null(rank)) {
+      Data <- Data %>%
+        group_by(.data[[case]]) %>%
+        mutate(.ms = mean(.data[[var]], na.rm = TRUE)) %>%
+        arrange(.data[[".ms"]]) %>%
+        nest() %>%
+        rowid_to_column(".rank") %>%
+        unnest(cols = .data$data) %>%
+        ungroup()
 
-  # if sel <= 1, assume percentage, convert to integer sel
-  if (between(sel, 0, 1)) {
-    sel <- round(sel * sel_max)
+      rank <- ".rank"
+    }
+
+    if (is.null(Data[[rank]])) stop("Rank variable '", rank, "' not present in Data.", call. = FALSE)
+
+    dir <- match.arg(dir, c("top", "bottom"))
+
+    Data <- switch(dir,
+      top = Data[Data[[rank]] <= sel, ],
+      bottom = Data[Data[[rank]] > sel_max - sel, ]
+    )
   }
 
   formula <- formula(paste0(var, " ~ 1 + (1 | ", case, ")"))
 
-  dir <- match.arg(dir, c("top", "bottom"))
-
-  Data <- switch(dir,
-    top = Data[Data[[rank]] <= sel, ],
-    bottom = Data[Data[[rank]] > sel_max - sel, ]
-  )
-
   model <- lmer(formula = formula, data = Data)
 
-  var_ratee <- as.numeric(VarCorr(model)[case])
-  var_resid <- sigma(model)^2
-  var_total <- var_ratee + var_resid
+  VarID <- as.numeric(VarCorr(model)[case])
+  VarResid <- sigma(model)^2
+  VarTotal <- VarID + VarResid
 
-  ICC1 <- var_ratee / var_total
-  ICC3 <- var_ratee / (var_ratee + var_resid / 3)
+  ICC1 <- VarID / VarTotal
+  ICC3 <- VarID / (VarID + VarResid / 3)
 
   bs <- bootMer(
     model,
@@ -109,18 +140,18 @@ ICCrestricted <- function(Data, case, var, rank = NULL,
   probs <- c(1 - ci, 1 + ci) / 2
 
   bICC1 <- bs[, 1] / rowSums(bs)
-  ICC1_CI <- quantile(bICC1, probs)
+  ICC1_CI <- quantile(bICC1, probs, names = FALSE)
 
   bICC3 <- bs[, 1] / (bs[, 1] + bs[, 2] / 3)
-  ICC3_CI <- quantile(bICC3, probs)
+  ICC3_CI <- quantile(bICC3, probs, names = FALSE)
 
   out <- data.frame(
     n_sel = sel,
     prop_sel = sel / sel_max,
-    dir,
-    var_ratee,
-    var_resid,
-    var_total,
+    dir = ifelse(sel == sel_max, NA, dir),
+    VarID,
+    VarResid,
+    VarTotal,
     ICC1,
     ICC1_LCI = ICC1_CI[1],
     ICC1_UCI = ICC1_CI[2],
@@ -128,7 +159,6 @@ ICCrestricted <- function(Data, case, var, rank = NULL,
     ICC3_LCI = ICC3_CI[1],
     ICC3_UCI = ICC3_CI[2]
   )
-  rownames(out) <- NULL
 
   out
 }
